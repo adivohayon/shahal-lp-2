56eb57c73e560671b33d6d2a53ae877844880ee3
Adiv Ohayon <adivohayon@gmail.com>
2017-06-12 18:30:13 +0300

    final fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,138 @@
 console.log('\'Allo \'Allo!');
 const index = app.templates.index();
 $('body').html(index);
-console.log(index);
+// console.log(index);
+// import swal from 'sweetalert2';
+$(document).ready(function() {
+	$('.form-success-container').hide();
+	function submitForm(formType) {
+		$('#' + formType + '-form').submit(function (e) {
+		    e.preventDefault();
+
+		    // $("input[type='submit']").addClass("loading");
+
+		    var name = $('#' + formType + '-name').val();
+		    var phone = $('#' + formType + '-phone').val();
+		    var phoneFirst = $('#' + formType + '-phone-first').val();
+		    var email = $('#' + formType + '-email').val();
+		    var go = true;
+
+		    if (!name) {
+		        go = false;
+		         $('#' + formType + '-name').addClass('error');
+		    } 
+		    if (!phone) {
+		        go = false;
+		        $('#' + formType + '-phone').addClass('error');
+		    }
+		    if (!phoneFirst) {
+		        go = false;
+		        $('#' + formType + '-phone-first').addClass('error');
+		    }
+
+		    var cvFile= $( '#file-upload' ).val();
+
+		    var ext = cvFile.split('.').pop();
+		    if(ext=="pdf" || ext=="docx" || ext=="doc"){
+		    	// console.log('aaaaa')
+		        
+		    } else {
+		    	go = false;
+		    	$('#' + formType + ' .upload-file').addClass('error');
+		    }
+
+		    
+		   
+		    if (!go){
+		        return;
+		    }
+			
+		    var index = name.indexOf(' ');  // Gets the first index where a space occours
+		    var firstName, lastName = '';
+			if (index == -1){
+				firstName = name; // Gets the first part
+				lastName = '';
+				
+			} else {
+				firstName = name.substr(0, index); // Gets the first part
+				lastName = name.substr(index + 1);
+			}
+			var fullPhone = phoneFirst + ' ' + phone;
+		    var $url = 'http://web2lead.shahal.co.il/web2lead/';  
+		    var $values = {
+		        FIRSTNAME: firstName,
+		        LASTNAME: lastName,
+		        EMAIL: email,
+		        PHONE1: fullPhone,
+		        CAMPAIGN_KEY: 'facebookms',
+		        REDIRECTURL: '',
+				CITYNAME: '',
+		        ERRORURL: '',
+		        numberOfNames: 5,
+		        Pid: 443,
+		        Sid: 67,
+		        strCAPTCHA: 'E301'
+
+		    };
+
+		    // $.get($url, $values).done(function (res) { 
+		    //     // fbq('track', 'shachal_lead');
+		    //     $('#' + formType + '-form .form-input-container').hide();
+		    //     $('#' + formType + '-form .form-success-container').show();
+		    // });
+
+		    // swal('נשלח בהצלחה');
+		    
+		    $('#' + formType + '-form .hide-on-success').hide();
+	        $('#' + formType + '-form .form-success-container').show();
+	        var files = $('#file-upload').prop('files');
+	        console.log(files);
+	        var data  = new FormData();
+	        data.append(cvFile, files[0]);
+
+		    var cv_url = 'http://dev.jetwebserver.com/shahal/postcv.php';
+
+		    console.log('cvFile', data);
+		    $.ajax({
+		        url: 'http://dev.jetwebserver.com/shahal/postcv.php',
+		        data: data,
+		        cache: false,
+		        contentType: 'multipart/form-data',
+		        processData: false,
+		        type: 'POST',
+		        success: function(data){
+		            console.log('success',data);
+		        },
+		        error: function(resp) {
+		        	console.log('error', resp);
+		        }
+		    });
+
+
+		    // $.post($cv_url, $mail_values).done(function (res) { });
+
+		    $('#' + formType + '-form').submit(function () {
+		        return;
+		    });
+		    e.preventDefault();
+		});
+	}
+
+	submitForm('desktop-lead');
+	submitForm('mobile-lead');
+	$('.upload-file-button').click(function(e) {
+		e.preventDefault();
+		$('#file-upload').trigger('click');
+	});
+
+
+	$('.send-cv').click(function(e) {
+		e.preventDefault();
+		var job = $(this).attr('data-job');
+		console.log(job);
+		$('#desktop-lead-job option[value=' + job +']').attr('selected','selected');
+		$('#desktop-lead-job').val(job);
+		$('#mobile-lead-job option[value=' + job +']').attr('selected','selected');
+		$('#mobile-lead-job').val(job);
+	});
+});
---
+++ postcv.php
@@ -0,0 +1,55 @@
+<?php
+
+$target_dir = "uploads_cv/";
+$target_file = $target_dir .uniqid('cv_')
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "0";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+
+$values = $_POST;
+
+$body = '';
+$body .= 'First Name : ' .$values['FIRSTNAME'] ."\r\n";
+$body .= 'Last Name : '  .$values['LASTNAME']."\r\n";
+$body .= 'Telephone : ' .$values['PHONE1']."\r\n";
+$body .= 'Email : ' .$values['EMAIL']."\r\n";
+$body .= 'APPROVE_MARKETING : ' .$values['APPROVE_MARKETING']."\r\n";
+$body .= 'APPROVE_CALL : ' .$values['APPROVE_CALL']."\r\n";
+$body .= 'Source :' . $values['CAMPAIGN_KEY'] ."\r\n";
+$body .= 'target_file :' . $target_file ."\r\n";
+
+
+
+$fp = fopen('cv_leads.csv', 'a+');
+
+fputcsv($fp, $values);
+
+fclose($fp);
+
+
+//mail ('editr@shahal.co.il, lizi@shahal.co.il, ravit@gofmans.co.il, liorg@shahal.co.il, oren+shahalleads@jetweb.co.il', 'New lead Shahal' , $body );
+mail ('oren@jetweb.co.il', 'New lead Shahal' , $body );
+exit;
+
+?>
